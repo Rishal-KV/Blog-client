3dee8b2270d804e26c824b11544c4537bc5dc8ac
Rishal-KV <rishkv923@gmail.com>
2024-07-19 02:21:26 +0530

    ui changes

--- src/API/axiosConfig.ts
+++ src/API/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"https//blogapp.nutrix.fun"
+    baseURL:"https://blogapp.nutrix.fun"
 })
 export default axiosInstance
